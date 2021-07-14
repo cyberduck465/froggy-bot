4b134e2c4b1c56408767f7498c9fa5ac2e801d14
hello1234316 <85028651+hello1234316@users.noreply.github.com>
2021-07-14 11:36:41 +0800

    Add files via upload

---
+++ message.js
@@ -0,0 +1,80 @@
+const { MessageEmbed } = require('discord.js');
+const { oneLine } = require('common-tags');
+
+module.exports = (client, message) => {
+  if (message.channel.type === 'dm' || !message.channel.viewable || message.author.bot) return;
+
+  // Get disabled commands
+  let disabledCommands = client.db.settings.selectDisabledCommands.pluck().get(message.guild.id) || [];
+  if (typeof(disabledCommands) === 'string') disabledCommands = disabledCommands.split(' ');
+  
+  // Get points
+  const { point_tracking: pointTracking, message_points: messagePoints, command_points: commandPoints } = 
+    client.db.settings.selectPoints.get(message.guild.id);
+
+  // Command handler
+  const prefix = client.db.settings.selectPrefix.pluck().get(message.guild.id);
+  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${prefix.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})\\s*`);
+
+  if (prefixRegex.test(message.content)) {
+
+    // Get mod channels
+    let modChannelIds = message.client.db.settings.selectModChannelIds.pluck().get(message.guild.id) || [];
+    if (typeof(modChannelIds) === 'string') modChannelIds = modChannelIds.split(' ');
+
+    const [, match] = message.content.match(prefixRegex);
+    const args = message.content.slice(match.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    let command = client.commands.get(cmd) || client.aliases.get(cmd); // If command not found, check aliases
+    if (command && !disabledCommands.includes(command.name)) {
+
+      // Check if mod channel
+      if (modChannelIds.includes(message.channel.id)) {
+        if (
+          command.type != client.types.MOD || (command.type == client.types.MOD && 
+          message.channel.permissionsFor(message.author).missing(command.userPermissions) != 0)
+        ) {
+          // Update points with messagePoints value
+          if (pointTracking)
+            client.db.users.updatePoints.run({ points: messagePoints }, message.author.id, message.guild.id);
+          return; // Return early so Froggy doesn't respond
+        }
+      }
+
+      // Check permissions
+      const permission = command.checkPermissions(message);
+      if (permission) {
+
+        // Update points with commandPoints value
+        if (pointTracking)
+          client.db.users.updatePoints.run({ points: commandPoints }, message.author.id, message.guild.id);
+        message.command = true; // Add flag for messageUpdate event
+        return command.run(message, args); // Run command
+      }
+    } else if ( 
+      (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) &&
+      message.channel.permissionsFor(message.guild.me).has(['SEND_MESSAGES', 'EMBED_LINKS']) &&
+      !modChannelIds.includes(message.channel.id)
+    ) {
+      const embed = new MessageEmbed()
+        .setTitle('嗨嗨，我是Froggy, 需要幫忙？')
+        .setThumbnail('https://raw.githubusercontent.com/sabattle/FroggyBot/develop/data/images/Froggy.png')
+        .setDescription(`你可以使用 \`${prefix}help\` 命令查看我可以執行的所有操作。`)
+        .addField('邀請我', oneLine`
+          你可以通過
+           [這裡](https://discord.com/api/oauth2/authorize?client_id=845889117187473419&permissions=0&scope=bot)邀請我!
+        `)
+        .addField('支援', oneLine`
+          如果你有任何問題、建議或發現錯誤，請加入
+           [Froggy支援服務器](https://discord.gg/8D39Y6fwFs)!
+        `)
+        .setFooter('由 Hello Phone 製作 awa')
+        .setColor(message.guild.me.displayHexColor);
+      message.channel.send(embed);
+    }
+  }
+
+  // Update points with messagePoints value
+  if (pointTracking) client.db.users.updatePoints.run({ points: messagePoints }, message.author.id, message.guild.id);
+};
+
